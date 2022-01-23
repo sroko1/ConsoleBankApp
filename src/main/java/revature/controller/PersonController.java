440d620212b943726be57d554adc5f29743a2182
sroko1 <70091227+sroko1@users.noreply.github.com>
2022-01-23 19:43:15 +0100

    Controllers  work fine

--- src/main/java/revature/controller/PersonController.java
+++ src/main/java/revature/controller/PersonController.java
@@ -1,2 +1,20 @@
-package revature.controller;public class PersonController {
+package revature.controller.person;
+
+import io.javalin.Javalin;
+import io.javalin.http.Handler;
+import revature.dao.PersonDao;
+import revature.model.Person;
+
+import java.sql.Connection;
+
+public class PersonController {
+
+
+    public PersonController(Javalin app) {
+        app.get("/persons", personHandler);
+    }
+    public Handler personHandler = ctx -> {
+        PersonView view = new PersonView();
+        ctx.html(view.render(new Person()));
+    };
 }
